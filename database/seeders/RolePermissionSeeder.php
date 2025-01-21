c76b99f8579eff1840622ff65a6e3d89a5dc9d53
DimiShenron <dimitry@shenron.fr>
2025-01-21 07:50:29 +0100

    add permissions and middlewares

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -12,75 +12,93 @@ class RolePermissionSeeder extends Seeder
     {
         // Création de permissions
         $permissions = [
-            'edit articles', 'delete articles', 'publish articles', 
+            'edit post', 'delete post', 'publish post', 'create post',
             'edit users', 'delete users', 'create users', 'view users',
-            'edit own profile', // Permission spécifique pour les actions sur le profil utilisateur
-            'view own profile',  // Permission pour voir son propre profil
-            'delete own profile', // Permission pour supprimer son propre profil
-            'manage users', 'assign roles'
+            'edit own profile', 'view own profile', 'delete own profile',
+            'manage users', 'assign roles', 'change post state',
+            'delete comment', 'create comment'
         ];
-
-
+        
         foreach ($permissions as $permission) {
             Permission::create(['name' => $permission]);
         }
-       
-    $userRole = Role::findOrCreate('user');
-    $adminRole = Role::findOrCreate('admin');
-    $superAdminRole = Role::findOrCreate('super admin');
-
-    $userRole->givePermissionTo([
-        'view own profile', 
-        'edit own profile', 
-        'delete own profile'
-    ]);
-
-    $adminRole->givePermissionTo([ 
-    'view own profile',
-    'edit own profile',
-    'delete users',
-    'edit users',
-    'view users',
-    'create users',
-    'manage users',
-        'edit articles',
-        'delete articles',
-        'publish articles'
-    ]);
-
-    $superAdminRole->givePermissionTo([
-    'assign roles',
-    'view own profile',
-    'edit own profile',
-    'delete users',
-    'edit users',
-    'view users',
-    'create users',
-    'delete own profile',
-        'edit articles',
-        'delete articles',
-        'publish articles'
-]);
-    //Auteur Role (a les memes droit qu'un user) + droit d'écriture d'articles
-    $authorRole = Role::findOrCreate('author');
-    $authorRole->givePermissionTo([
-        'view own profile',
-        'edit own profile',
-        'delete own profile',
-        'edit articles',
-        'publish articles'
-    ]);
-    
-    // Role Modérateur (a les memes droits qu'un autheur) + droit de suppression d'articles
-    $moderatorRole = Role::findOrCreate('moderator');
-    $moderatorRole->givePermissionTo([
-        'view own profile',
-        'edit own profile',
-        'delete own profile',
-        'edit articles',
-        'delete articles',
-        'publish articles'
-    ]);
+        
+        $userRole = Role::findOrCreate('user');
+        $adminRole = Role::findOrCreate('admin');
+        $superAdminRole = Role::findOrCreate('super admin');
+        
+        // Utilisateur de base
+        $userRole->givePermissionTo([
+            'view own profile', 
+            'edit own profile', 
+            'delete own profile',
+            'create comment',  // Peut créer des commentaires
+            'delete comment'   // Peut supprimer ses propres commentaires
+        ]);
+        
+        // Administrateur
+        $adminRole->givePermissionTo([ 
+            'view own profile',
+            'edit own profile',
+            'delete users',
+            'edit users',
+            'view users',
+            'create users',
+            'manage users',
+            'edit post',
+            'delete post',
+            'publish post',
+            'create comment',
+            'delete comment',
+            'change post state'
+        ]);
+        
+        // Super Admin
+        $superAdminRole->givePermissionTo([
+            'assign roles',
+            'view own profile',
+            'edit own profile',
+            'delete users',
+            'edit users',
+            'view users',
+            'create users',
+            'delete own profile',
+            'edit post',
+            'delete post',
+            'publish post',
+            'create post',
+            'create comment',
+            'delete comment',
+            'change post state',
+            'manage users'
+        ]);
+        
+        // Auteur
+        $authorRole = Role::findOrCreate('author');
+        $authorRole->givePermissionTo([
+            'view own profile',
+            'edit own profile',
+            'delete own profile',
+            'create post',     // Peut créer des posts
+            'edit post',       // Peut éditer ses propres posts
+            'publish post',    // Peut publier ses posts
+            'create comment',  // Peut commenter
+            'delete comment'   // Peut supprimer ses commentaires
+        ]);
+        
+        // Modérateur
+        $moderatorRole = Role::findOrCreate('moderator');
+        $moderatorRole->givePermissionTo([
+            'view own profile',
+            'edit own profile',
+            'delete own profile',
+            'edit post',       // Peut éditer tous les posts
+            'delete post',     // Peut supprimer tous les posts
+            'publish post',    // Peut publier/dépublier les posts
+            'change post state',
+            'create comment',
+            'delete comment'   // Peut supprimer tous les commentaires
+        ]);
 
         // Ici, vous pourriez logiquement restreindre l'utilisateur à n'agir que sur son propre profil via la logique de l'application,
         // en utilisant par exemple un middleware ou des vérifications dans les contrôleurs.
--- routes/api.php
+++ routes/api.php
@@ -18,60 +18,94 @@ use App\Http\Controllers\API\CommentController;
 |
 */
 
+
 // Auth routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-// ROUTES A CLASSER AU BON ENDROIT
-Route::apiResource('posts', PostController::class);
-Route::apiResource('comments', CommentController::class);
-Route::post('posts/{post}/comment', [PostController::class, 'commenter']);
+// Routes publiques pour voir les posts et commentaires
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::get('/comments', [CommentController::class, 'index']);
+Route::get('/comments/{comment}', [CommentController::class, 'show']);
 
 // Routes pour les utilisateurs authentifiés
 Route::middleware('auth:sanctum')->group(function () {
-    // Routes accessibles par le rôle 'user'
-    Route::middleware('role:user|super admin')->group(function () {
-        Route::get('/user/profile', [UserController::class, 'profile'])->middleware('permission:view own profile');
-        Route::put('/user/profile', [UserController::class, 'updateProfile'])->middleware('permission:edit own profile');
-        Route::delete('/user/profile', [UserController::class, 'deleteProfile'])->middleware('permission:delete own profile');
-    });
+    // Routes pour tous les utilisateurs authentifiés
+    Route::middleware('role:user')->group(function () {
+        // Profile routes
+        Route::get('/user/profile', [UserController::class, 'profile'])
+            ->middleware('permission:view own profile');
+        Route::put('/user/profile', [UserController::class, 'updateProfile'])
+            ->middleware('permission:edit own profile');
+        Route::delete('/user/profile', [UserController::class, 'deleteProfile'])
+            ->middleware('permission:delete own profile');
 
-    Route::middleware('role:admin')->group(function () {
-        Route::get('/user/profile', [UserController::class, 'profile'])->middleware('permission:view own profile');
-        Route::put('/user/profile', [UserController::class, 'updateProfile'])->middleware('permission:edit own profile');   
-        Route::get('/users', [UserController::class, 'index'])->middleware('permission:view users');
-        Route::post('/users', [UserController::class, 'store'])->middleware('permission:create users');
-        Route::get('/users/{id}', [UserController::class, 'show'])->middleware('permission:view users');
-        Route::put('/users/{id}', [UserController::class, 'update'])->middleware('permission:edit users');
-        Route::delete('/users/{id}', [UserController::class, 'delete'])->middleware('permission:delete users');
-        Route::get('/role/{user_id}', [UserController::class, 'adminAssignRole'])->middleware('permission:manage users');
-    });
-
-    // Routes spécifiques au super administrateur
-    Route::middleware('role:super admin')->group(function () {
-        Route::get('/users', [UserController::class, 'index'])->middleware('permission:view users');
-        Route::post('/users', [UserController::class, 'store'])->middleware('permission:create users');
-        Route::get('/users/{id}', [UserController::class, 'show'])->middleware('permission:view users');
-        Route::put('/admin/role/{user_id}', [UserController::class, 'SuperAssignRole'])->middleware('permission:assign roles');
-        Route::delete('/admin/{user_id}', [UserController::class, 'destroy'])->middleware('permission:delete users');
-        Route::put('/admin/{user_id}', [UserController::class, 'SuperUpdate'])->middleware('permission:edit users');
-    });
-
-        // Routes pour les modérateurs
-    Route::middleware('role:moderator')->group(function () {
-        Route::get('/moderator/users', [UserController::class, 'index'])->middleware('permission:view users');
-        Route::get('/moderator/users/{id}', [UserController::class, 'show'])->middleware('permission:view users');
+        // Commentaires routes
+        Route::post('/posts/{post}/comments', [CommentController::class, 'store'])
+            ->middleware('permission:create comment');
+        Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])
+            ->middleware('permission:delete comment');
     });
 
     // Routes pour les auteurs
     Route::middleware('role:author')->group(function () {
-        Route::get('/author/users', [UserController::class, 'index'])->middleware('permission:view users');
-        Route::get('/author/users/{id}', [UserController::class, 'show'])->middleware('permission:view users');
+        Route::post('/posts', [PostController::class, 'store'])
+            ->middleware('permission:create post');
+        Route::put('/posts/{post}', [PostController::class, 'update'])
+            ->middleware('permission:edit post');
+        Route::post('/posts/{post}/publish', [PostController::class, 'publish'])
+            ->middleware('permission:publish post');
     });
 
+    // Routes pour les modérateurs
+    Route::middleware('role:moderator')->group(function () {
+        Route::put('/posts/{post}', [PostController::class, 'update'])
+            ->middleware('permission:edit post');
+        Route::delete('/posts/{post}', [PostController::class, 'destroy'])
+            ->middleware('permission:delete post');
+        Route::put('/posts/{post}/state', [PostController::class, 'changeState'])
+            ->middleware('permission:change post state');
+        Route::delete('/comments/{comment}', [CommentController::class, 'moderatorDestroy'])
+            ->middleware('permission:delete comment');
+    });
 
+    // Routes pour les administrateurs
+    Route::middleware('role:admin')->group(function () {
+        // Gestion des utilisateurs
+        Route::get('/users', [UserController::class, 'index'])
+            ->middleware('permission:view users');
+        Route::post('/users', [UserController::class, 'store'])
+            ->middleware('permission:create users');
+        Route::get('/users/{id}', [UserController::class, 'show'])
+            ->middleware('permission:view users');
+        Route::put('/users/{id}', [UserController::class, 'update'])
+            ->middleware('permission:edit users');
+        Route::delete('/users/{id}', [UserController::class, 'delete'])
+            ->middleware('permission:delete users');
+        Route::put('/users/{user_id}/role', [UserController::class, 'adminAssignRole'])
+            ->middleware('permission:manage users');
+        
+        // Gestion des posts
+        Route::post('/posts', [PostController::class, 'store'])
+            ->middleware('permission:create post');
+        Route::put('/posts/{post}', [PostController::class, 'update'])
+            ->middleware('permission:edit post');
+        Route::delete('/posts/{post}', [PostController::class, 'destroy'])
+            ->middleware('permission:delete post');
+    });
 
-    // Routes liées à la création, modification et suppression d'articles pour tout les utilisateurs
-
+    // Routes pour le super admin
+    Route::middleware('role:super admin')->group(function () {
+        // Gestion avancée des utilisateurs
+        Route::put('/admin/role/{user_id}', [UserController::class, 'SuperAssignRole'])
+            ->middleware('permission:assign roles');
+        Route::delete('/admin/{user_id}', [UserController::class, 'destroy'])
+            ->middleware('permission:delete users');
+        Route::put('/admin/{user_id}', [UserController::class, 'SuperUpdate'])
+            ->middleware('permission:edit users');
+        
+        // Le super admin hérite de toutes les autres permissions via le système de rôles
+    });
 });
